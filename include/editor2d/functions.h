347693b063de89b5b316350b35b3237d002ab100
David Ortuño <nixwod@gmail.com>
2006-06-28 19:04:37 +0000

    Añadida funcion de busqueda de sub-directorios

--- include/editor2d/functions.h
+++ include/editor2d/functions.h
@@ -2,6 +2,13 @@
 #define __FUNCTIONS_H__
 
 #include <SDL.h>
+#include <string>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <list>
+
+using namespace std;
 
 typedef struct
 {
@@ -9,6 +16,8 @@ typedef struct
     SDL_Rect window;
     SDL_Surface * dst;
 } Blit_Task;
+
+list<string> Get_Directories(char*);
 
 //Blit_Task* New_Blit_Task(SDL_Surface * some_src, SDL_Rect &some_window, SDL_Surface * some_dst);
 
--- src/editor2d/functions.cpp
+++ src/editor2d/functions.cpp
@@ -12,3 +12,47 @@ Blit_Task* New_Blit_Task(SDL_Surface * some_src, SDL_Rect &some_window, SDL_Surf
     return bt;
 }
 
+list<string> Get_Directories(char* dir_work)
+{
+    char dirchain[300];
+    list<string> list_directories;
+
+    string path_work = getcwd(dirchain,300);
+    string full_path = path_work + "\\" + dir_work;
+
+    DIR *pdir;
+    struct dirent *pent;
+    struct stat dataFile;
+
+    pdir=opendir(full_path.c_str());
+    if (!pdir)
+    {
+        printf ("opendir() failure; terminating");
+        exit(1);
+    }
+    errno=0;
+    while ((pent=readdir(pdir)))
+    {
+        string name = pent->d_name;
+        string path_completly = full_path + "\\" + name;
+
+        if (stat(path_completly.c_str(), &dataFile) == -1)
+            printf ("not exist directory\n");
+        else
+        {
+            if (S_ISDIR(dataFile.st_mode) && name != "." && name != ".." && name != "CVS")
+                list_directories.push_back(path_completly);
+                //printf("%s\n", pent->d_name); // Variable a almacenar -- path_completly --
+        }
+    }
+
+    if (errno)
+    {
+        printf ("readdir() failure; terminating");
+        exit(1);
+    }
+    closedir(pdir);
+
+    return list_directories;
+}
+
